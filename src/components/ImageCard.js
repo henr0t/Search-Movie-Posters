75418b2f505aa836b391a6800f8c7e4aaabf50a0
henr0t <h.trieu@live.nl>
2020-10-23 10:31:42 +0200

    hover effects

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -16,17 +16,18 @@ class ImageCard extends React.Component {
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
 
-    const spans = Math.ceil(height / 15 + 1);
+    const spans = Math.ceil(height / 15);
     this.setState({ spans });
   };
 
   render() {
-    const { original_title, poster_path } = this.props.img;
+    const { original_title, poster_path, overview } = this.props.img;
 
     return (
       <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
           ref={this.imageRef}
+          title={original_title + ": " + overview}
           alt={original_title}
           src={"http://image.tmdb.org/t/p/w500/" + poster_path}
         />
